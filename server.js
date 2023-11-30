b1338a91b9579129d7d731bfbde1c7d56a098dfb
gallivas <gallivas@oregonstate.edu>
2023-11-30 11:20:38 -0500

    Reduced microservice to essentials

--- server.js
+++ server.js
@@ -13,46 +13,6 @@ app.use(express.urlencoded({
 }));
 
 console.log(process.env);
-/**
- * Route handler for POST request to the URL /hello
- */
-app.post("/hello", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    res.send(`Hi ${name}. Your email is ${email}. The request was sent using the HTTP method POST`);
-});
-
-/**
- * Route handler for GET request to the URL /hello
- */
-app.get("/hello", (req, res) => {
-    const name = req.query.name;
-    const email = req.query.email;
-    res.send(`Hi ${name}. Your email is ${email}. The request was sent using the HTTP method GET`);
-});
-
-/**
- * Route handler for POST request to the URL /login
- */
-app.post("/login", (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    res.redirect('/connect.html')
-});
-
-/**
- * Route handler for POST request to the URL /create-playlist
- */
-app.post("/create-playlist", (req, res) => {
-    res.redirect('/playlist.html');
-});
-
-/**
- * Route handler for POST request to the URL /connectmusic
- */
-app.post("/connect-music", (req, res) => {
-    res.redirect('/home.html')
-});
 
 
 app.listen(PORT, () => {
