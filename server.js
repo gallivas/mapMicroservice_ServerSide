d742f8f89dcd2a8034f081a1a88ea47c669007d4
gallivas <gallivas@oregonstate.edu>
2023-11-30 13:30:19 -0500

    rearranging file structure

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-'use strict';
+// 'use strict';
 
-// NOTE: Don't change the port number
-const PORT = 3000;
+// // NOTE: Don't change the port number
+// const PORT = 3000;
 
-const express = require("express");
-const app = express();
-require('dotenv').config();
-app.use(express.static('public'));
+// const express = require("express");
+// const app = express();
+// require('dotenv').config();
+// app.use(express.static('public'));
 
-app.use(express.urlencoded({
-    extended: true
-}));
+// app.use(express.urlencoded({
+//     extended: true
+// }));
 
-console.log(process.env);
+// console.log(process.env);
 
 
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}...`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server listening on port ${PORT}...`);
+// });
